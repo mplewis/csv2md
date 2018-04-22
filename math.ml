8b471ed03456296252fccee2f5071d1738d577a9
Matt Lewis <matt@mplewis.com>
2018-04-22 17:46:54 -0400

    working spec

--- math.ml
+++ math.ml
@@ -1,3 +1,5 @@
 open Batteries
 
 let add a b = a + b
+
+let sub a b = a - b
--- math.mli
+++ math.mli
@@ -1,2 +1,5 @@
 (* Add two numbers *)
 val add : int -> int -> int
+
+(* Subtract two numbers *)
+val sub : int -> int -> int
